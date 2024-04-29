d8850dd20e96e78d2e663e072ab7d153e7938c09
hyunjung <gusejddl519@naver.com>
2024-04-29 14:17:27 +0900

    valid check

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,2 +1,26 @@
 public class SimilarityChecker {
+
+
+    public void check(String str1, String str2) {
+        assertIllegalArgument(str1,str2);
+    }
+
+    private static void assertIllegalArgument(String str1,String str2) {
+        if (str1 == null || str2 == null) {
+            throw new IllegalArgumentException();
+        }
+
+        for (char number : str1.toCharArray()) {
+            if (number >= '0' || number <= '9') {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        for (char number : str2.toCharArray()) {
+            if (number >= '0' || number <= '9') {
+                throw new IllegalArgumentException();
+            }
+        }
+
+    }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,5 +1,25 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityCheckerTest {
 
+
+    @Test
+    void throwExceptionWhenInputInvalidInput() {
+        assertIllegalArgument(null,null);
+        assertIllegalArgument("12d","1asdg");
+        assertIllegalArgument("1234","asdg");
+        assertIllegalArgument(null,"s");
+    }
+
+    private void assertIllegalArgument(String str1,String str2) {
+        try {
+            SimilarityChecker similarityChecker = new SimilarityChecker();
+            similarityChecker.check(str1,str2);
+            fail();
+        } catch (IllegalArgumentException e) {
+
+        }
+    }
 }
